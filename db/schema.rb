9db26ed08af8628e37df497e770047b4d54cfb3f
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-09 16:49:49 -0500

    add user reference to groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_212541) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_214823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_212541) do
     t.float "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -85,4 +87,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_212541) do
   add_foreign_key "connections", "deals"
   add_foreign_key "connections", "groups"
   add_foreign_key "deals", "users"
+  add_foreign_key "groups", "users"
 end
