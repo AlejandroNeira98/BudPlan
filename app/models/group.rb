7ace6b73eec47f123f6dfd350e6bb9cdba494b04
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-09 17:17:48 -0500

    add validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
+  validates :name, precense: true
+  has_one_attached :icon
+  validates :icon, precense:true
   belongs_to :user
   has_and_belongs_to_many :deals
 end
