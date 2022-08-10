1ade4dbb3680f83b9e2e2641e946affe40f4a86e
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-10 11:47:19 -0500

    add test to group model

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    user = User.create(name: "some name", email:"some@email.com", password: "some password")
+    subject = Group.new(name: "some category", user_id: user.id)
+    subject.icon.attach(io: File.open("#{Rails.root}/app/assets/images/image.jpg"), filename: 'image.jpg', content_type: 'image/jpg')
+  end
+
+  after(:all) do
+    user.destroy
+    subject.destroy
+  end
+
+  it 'should be valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'icon should be present' do
+    subject.icon = nil
+    expect(subject).to_not be_valid
+  end
 end
