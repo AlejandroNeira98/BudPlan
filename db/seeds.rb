b3c4f261bc6fdb54f12d32bbec86ae7a773ac0eb
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-10 12:41:07 -0500

    Create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+@user = User.create(name: "some name", email:"some@email.com", password: "some password")
+
+@subject = Group.new(name: "some category", user_id: @user.id)
+@subject.icon.attach(io: File.open("#{Rails.root}/app/assets/images/image.jpg"), filename: 'image.jpg')
+@subject.save
+
+@deal = Deal.new(name: "some deal", amount: 100, user_id: @user.id)
+@deal.groups << @subject
+@deal.save
+
