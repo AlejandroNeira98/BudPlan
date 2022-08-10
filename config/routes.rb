b4b53c58f18004e1e380f32f220a77da17200358
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-10 12:54:04 -0500

    Create controller and add authentication restrains

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'deals/new'
+  get 'groups/index'
+  get 'groups/new'
+  get 'groups/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
