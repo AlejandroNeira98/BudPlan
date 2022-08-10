9850d39f1bd62e53fe84d0ae3487ecfe05c14b1a
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-10 12:11:19 -0500

    re run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -42,15 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_220157) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "connections", force: :cascade do |t|
-    t.bigint "deal_id", null: false
-    t.bigint "group_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["deal_id"], name: "index_connections_on_deal_id"
-    t.index ["group_id"], name: "index_connections_on_group_id"
-  end
-
   create_table "deals", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
@@ -89,8 +80,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_220157) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "connections", "deals"
-  add_foreign_key "connections", "groups"
   add_foreign_key "deals", "users"
   add_foreign_key "groups", "users"
 end
