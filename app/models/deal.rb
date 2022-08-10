aae6eb749c375918706123d94d0b2f2564835d0c
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-10 10:32:51 -0500

    fix linters

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,9 +2,9 @@ class Deal < ApplicationRecord
   validates :name, :ammount, precense: true
   belongs_to :user
   has_and_belongs_to_many :groups
-  validate :has_group
+  validate :group?
 
-  def has_group
-    errors.add(:base, 'must add at least one category') if self.groups.empty?
+  def group?
+    errors.add(:base, 'must add at least one category') if groups.empty?
   end
 end
