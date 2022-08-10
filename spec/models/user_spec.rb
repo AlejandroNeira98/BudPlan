7dc95fb24375954cd339bfc30e21848c5b49e5cb
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-10 11:43:38 -0500

    Create user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: "some name", email:"some@email.com", password: "some password")}
+  before { subject.save }
+
+  it 'Name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'email should be present' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'password should be present' do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
 end
