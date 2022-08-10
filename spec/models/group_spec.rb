55771fa3611474bcb4fabf9cee8d177221442125
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-10 12:11:47 -0500

    finish group test

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,27 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Group, type: :model do
   before(:all) do
-    user = User.create(name: "some name", email:"some@email.com", password: "some password")
-    subject = Group.new(name: "some category", user_id: user.id)
-    subject.icon.attach(io: File.open("#{Rails.root}/app/assets/images/image.jpg"), filename: 'image.jpg', content_type: 'image/jpg')
+    @user = User.create(id: 1, name: "some name", email:"some@email.com", password: "some password")
+    @subject = Group.new(name: "some category", user_id: @user.id)
+    @subject.icon.attach(io: File.open("#{Rails.root}/app/assets/images/image.jpg"), filename: 'image.jpg')
+    @subject.save
   end
 
   after(:all) do
-    user.destroy
-    subject.destroy
+    @user.delete
+    @subject.delete
   end
 
   it 'should be valid' do
-    expect(subject).to be_valid
+    expect(@subject).to be_valid
   end
 
   it 'name should be present' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+    @subject.name = nil
+    expect(@subject).to_not be_valid
   end
 
   it 'icon should be present' do
-    subject.icon = nil
-    expect(subject).to_not be_valid
+    @subject.icon = nil
+    expect(@subject).to_not be_valid
   end
 end
