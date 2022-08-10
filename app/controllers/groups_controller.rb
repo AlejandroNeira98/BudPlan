fe8dddd009703852dd83d38ac405b740cdc4668e
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-10 16:37:28 -0500

    Update groups controller, views and routes

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,14 @@
 class GroupsController < ApplicationController
   def index
+    @categories = current_user.groups
   end
 
   def new
   end
 
   def show
+    id = params[:id]
+    @category = Group.find(id)
+    @deals = category.deals.order('deals.created_at DESC')
   end
 end
