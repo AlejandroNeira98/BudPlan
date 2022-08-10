32d29002271470d4bb9f598041e637cfaa84b300
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-10 11:43:12 -0500

    fix validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
-  validates :name, precense: true
+  validates :name, :presence => true
   has_one_attached :icon
-  validates :icon, precense:true
+  validates :icon, :attached => true
   belongs_to :user
   has_and_belongs_to_many :deals
 end
